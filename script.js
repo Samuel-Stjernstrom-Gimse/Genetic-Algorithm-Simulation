62e9fc6c83ba9244f542eec318b74331ff63870c
Zam <samuel.kodehode@gmail.com>
2024-02-27 22:24:30 +0100

    2.0.0

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 "use strict";
-function main(generations) {
+function main(generations, genSize, mutation) {
     const dataArray = [];
     const objArray = [];
-    const genCount = document.getElementById('genCount');
     let bestDistance = Infinity;
     let bestDistanceIndex = Infinity;
     const canvas = document.getElementById('canvas');
@@ -13,52 +12,63 @@ function main(generations) {
         ctx.fillStyle = color;
         ctx.fillRect(x, y, w, h);
     };
+    function drawStrokedCircle(ctx, x, y, radius, fillColor) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+        ctx.stroke();
+    }
     function calculateDistance(x1, y1, x2, y2) {
         return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
     }
     for (let i = 0; i < 10; i++) {
         const squareArray = [];
-        for (let j = 0; j < 10000; j++) {
+        for (let j = 0; j < genSize; j++) {
             const getRandom = Math.floor(Math.random() * 4) + 1;
             squareArray.push(getRandom);
         }
         let squareObj = {
-            xPosition: 250,
-            yPosition: 250,
-            distanceFromGoal: 0,
+            xPosition: canvas.width / 2,
+            yPosition: canvas.height / 2,
+            distanceFromGoal: 0
         };
         objArray.push(squareObj);
         dataArray.push(squareArray);
     }
     function initLearning(ctx, numGenerations) {
         let generationCounter = 0;
-        const learningInterval = setInterval(() => {
-            if (bestDistance < 2) {
+        let lastFrameTime = 0;
+        const fpsInterval = 1000 / 30;
+        const animate = (timestamp) => {
+            if (bestDistance < 7 || generationCounter >= numGenerations) {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                bestDistance = Infinity;
-                main(1000);
+                main(Infinity, 2000, 20);
+                return;
             }
-            if (generationCounter < numGenerations) {
+            const elapsedTime = timestamp - lastFrameTime;
+            if (elapsedTime > fpsInterval) {
+                lastFrameTime = timestamp - (elapsedTime % fpsInterval);
                 objArray.forEach((obj) => {
-                    obj.xPosition = 250;
-                    obj.yPosition = 250;
+                    obj.xPosition = canvas.width / 2;
+                    obj.yPosition = canvas.height / 2;
                 });
                 dataArray.forEach((array, rowIndex) => {
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
                     array.forEach((value) => {
                         if (value === 1) {
-                            objArray[rowIndex].xPosition -= 1;
+                            objArray[rowIndex].xPosition -= 4;
                         }
                         else if (value === 2) {
-                            objArray[rowIndex].yPosition -= 1;
+                            objArray[rowIndex].yPosition -= 4;
                         }
                         else if (value === 3) {
-                            objArray[rowIndex].xPosition += 1;
+                            objArray[rowIndex].xPosition += 4;
                         }
                         else if (value === 4) {
-                            objArray[rowIndex].yPosition += 1;
+                            objArray[rowIndex].yPosition += 4;
                         }
-                        render(objArray[rowIndex].xPosition, objArray[rowIndex].yPosition, gX, gY, ctx);
+                        render(objArray[rowIndex].xPosition, objArray[rowIndex].yPosition, gX, gY, ctx, generationCounter);
                     });
                 });
                 bestDistance = Infinity;
@@ -76,13 +86,11 @@ function main(generations) {
                             array[valueIndex] = dataArray[bestDistanceIndex][valueIndex];
                         });
                     }
-                    const randomIndices = Array.from({ length: 10 }, () => Math.floor(Math.random() * array.length));
+                    const randomIndices = Array.from({ length: mutation }, () => Math.floor(Math.random() * array.length));
                     randomIndices.forEach((randomIndex) => {
                         array[randomIndex] = Math.floor(Math.random() * 4) + 1;
                     });
                 });
-                bestDistance = Infinity;
-                bestDistance = Infinity;
                 objArray.forEach((obj, index) => {
                     obj.distanceFromGoal = calculateDistance(obj.xPosition, obj.yPosition, gX, gY);
                     if (obj.distanceFromGoal < bestDistance) {
@@ -92,16 +100,19 @@ function main(generations) {
                 });
                 generationCounter++;
             }
-            else {
-                clearInterval(learningInterval);
-            }
-        }, 100);
+            requestAnimationFrame(animate);
+        };
+        requestAnimationFrame(animate);
     }
-    function render(x, y, gx, gy, ctx) {
-        draw(x, y, 2, 2, 'red', ctx);
-        draw(gx, gy, 10, 10, 'green', ctx);
+    function render(x, y, gx, gy, ctx, generationCounter) {
+        draw(x, y, 1, 1, 'white', ctx);
+        drawStrokedCircle(ctx, gx, gy, 5, 'orange');
+        ctx.font = '16px Arial';
+        ctx.fillStyle = 'white';
+        ctx.fillText(`Generations: ${generationCounter}`, 10, 20);
     }
-    initLearning(ctx, generations);
+    if (ctx !== null)
+        initLearning(ctx, generations);
 }
-main(1000);
+main(Infinity, 2000, 20);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,135 +1,151 @@
-function main(generations: number){
-	const dataArray: Array<number>[] = [];
-	const objArray: any[] = [];
-	const genCount = document.getElementById('genCount') as HTMLHeadingElement
+function main(generations: number, genSize: number, mutation: number) {
+	const dataArray: Array<number>[] = []
+	const objArray: any[] = []
+
 	let bestDistance = Infinity
 	let bestDistanceIndex = Infinity
 
-	const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-
-	const ctx = canvas.getContext('2d');
-
-	const gX = Math.floor(Math.random() * canvas.width - 30);
-	const gY = Math.floor(Math.random() * canvas.height - 30);
+	const canvas = document.getElementById('canvas') as HTMLCanvasElement
+	const ctx = canvas.getContext('2d')
+	const gX = Math.floor(Math.random() * canvas.width - 30)
+	const gY = Math.floor(Math.random() * canvas.height - 30)
 
 	const draw = (x: number, y: number, w: number, h: number, color: string, ctx: any): void => {
-		ctx.fillStyle = color;
-		ctx.fillRect(x, y, w, h);
-	};
+		ctx.fillStyle = color
+		ctx.fillRect(x, y, w, h)
+	}
+
+	function drawStrokedCircle(
+		ctx: CanvasRenderingContext2D,
+		x: number,
+		y: number,
+		radius: number,
+		fillColor: string
+	): void {
+		ctx.beginPath()
+		ctx.arc(x, y, radius, 0, 2 * Math.PI)
+		ctx.fillStyle = fillColor
+		ctx.fill()
+		ctx.stroke()
+	}
 
 	function calculateDistance(x1: number, y1: number, x2: number, y2: number): number {
 		// Euclidean distance formula
-		return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+		return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 	}
 
 	for (let i = 0; i < 10; i++) {
-		const squareArray = [];
-		for (let j = 0; j < 10000; j++) {
-			const getRandom = Math.floor(Math.random() * 4) + 1;
-			squareArray.push(getRandom);
+		const squareArray = []
+		for (let j = 0; j < genSize; j++) {
+			const getRandom = Math.floor(Math.random() * 4) + 1
+			squareArray.push(getRandom)
 		}
 
 		let squareObj = {
-			xPosition: 250,
-			yPosition: 250,
-			distanceFromGoal: 0,
-		};
+			xPosition: canvas.width / 2,
+			yPosition: canvas.height / 2,
+			distanceFromGoal: 0
+		}
 
-		objArray.push(squareObj);
-		dataArray.push(squareArray);
+		objArray.push(squareObj)
+		dataArray.push(squareArray)
 	}
 
-	function initLearning(ctx: any, numGenerations: number) {
-		let generationCounter = 0;
+	// @ts-ignore
 
-		const learningInterval = setInterval(() => {
-			if(bestDistance < 2){
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-				bestDistance = Infinity
-				main(1000)
+	function initLearning(ctx: CanvasRenderingContext2D, numGenerations: number) {
+		let generationCounter = 0
+		let lastFrameTime = 0
+		const fpsInterval = 1000 / 30
+
+		const animate = (timestamp: number) => {
+			if (bestDistance < 7 || generationCounter >= numGenerations) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height)
+				main(Infinity, 2000, 20)
+				return
 			}
-			if (generationCounter < numGenerations) {
+
+			const elapsedTime = timestamp - lastFrameTime
+			if (elapsedTime > fpsInterval) {
+				lastFrameTime = timestamp - (elapsedTime % fpsInterval)
+
 				objArray.forEach((obj) => {
-					obj.xPosition = 250;
-					obj.yPosition = 250;
-				});
+					obj.xPosition = canvas.width / 2
+					obj.yPosition = canvas.height / 2
+				})
 
 				dataArray.forEach((array, rowIndex) => {
-					ctx.clearRect(0,0, canvas.width, canvas.height)
+					ctx.clearRect(0, 0, canvas.width, canvas.height)
 					array.forEach((value) => {
 						if (value === 1) {
-							objArray[rowIndex].xPosition -= 1;
+							objArray[rowIndex].xPosition -= 4
 						} else if (value === 2) {
-							objArray[rowIndex].yPosition -= 1;
+							objArray[rowIndex].yPosition -= 4
 						} else if (value === 3) {
-							objArray[rowIndex].xPosition += 1;
+							objArray[rowIndex].xPosition += 4
 						} else if (value === 4) {
-							objArray[rowIndex].yPosition += 1;
+							objArray[rowIndex].yPosition += 4
 						}
-					/*	objArray.forEach((obj) => {
-							render(obj.xPosition, obj.yPosition, gX, gY, ctx);
-						});*/
-						render(objArray[rowIndex].xPosition, objArray[rowIndex].yPosition, gX, gY, ctx)
-					});
+						render(
+							objArray[rowIndex].xPosition,
+							objArray[rowIndex].yPosition,
+							gX,
+							gY,
+							ctx,
+							generationCounter
+						)
+					})
+				})
 
-				});
-
-				bestDistance = Infinity;
-				bestDistanceIndex = 0;
+				bestDistance = Infinity
+				bestDistanceIndex = 0
 				objArray.forEach((obj, index) => {
-
-					obj.distanceFromGoal = calculateDistance(obj.xPosition, obj.yPosition, gX, gY);
+					obj.distanceFromGoal = calculateDistance(obj.xPosition, obj.yPosition, gX, gY)
 					if (obj.distanceFromGoal < bestDistance) {
-						bestDistance = obj.distanceFromGoal;
-						bestDistanceIndex = index;
+						bestDistance = obj.distanceFromGoal
+						bestDistanceIndex = index
 					}
-				});
+				})
 
 				dataArray.forEach((array, rowIndex) => {
 					if (rowIndex !== bestDistanceIndex) {
 						array.forEach((value, valueIndex) => {
-							array[valueIndex] = dataArray[bestDistanceIndex][valueIndex];
-						});
+							array[valueIndex] = dataArray[bestDistanceIndex][valueIndex]
+						})
 					}
 
-					const randomIndices = Array.from({ length: 10 }, () => Math.floor(Math.random() * array.length));
+					const randomIndices = Array.from({ length: mutation }, () =>
+						Math.floor(Math.random() * array.length)
+					)
 					randomIndices.forEach((randomIndex) => {
-						array[randomIndex] = Math.floor(Math.random() * 4) + 1;
-					});
+						array[randomIndex] = Math.floor(Math.random() * 4) + 1
+					})
+				})
 
-				});
-
-
-
-				bestDistance = Infinity;
-				bestDistance = Infinity;
 				objArray.forEach((obj, index) => {
-					obj.distanceFromGoal = calculateDistance(obj.xPosition, obj.yPosition, gX, gY);
+					obj.distanceFromGoal = calculateDistance(obj.xPosition, obj.yPosition, gX, gY)
 
 					if (obj.distanceFromGoal < bestDistance) {
-						bestDistance = obj.distanceFromGoal;
-						bestDistanceIndex = index;
+						bestDistance = obj.distanceFromGoal
+						bestDistanceIndex = index
 					}
-
-
-				});
-
-				generationCounter++;
-			} else {
-				clearInterval(learningInterval);
+				})
+				generationCounter++
 			}
-		}, 100); // Set the interval time (in milliseconds)
+
+			requestAnimationFrame(animate)
+		}
+		requestAnimationFrame(animate)
 	}
 
-	function render(x: number,y: number,gx: number,gy: number, ctx: any){
-		draw(x,y,2,2,'red',ctx)
-		draw(gx,gy,10,10,'green',ctx)
-
+	function render(x: number, y: number, gx: number, gy: number, ctx: any, generationCounter: number) {
+		draw(x, y, 1, 1, 'white', ctx)
+		drawStrokedCircle(ctx, gx, gy, 5, 'orange')
+		ctx.font = '16px Arial'
+		ctx.fillStyle = 'white'
+		ctx.fillText(`Generations: ${generationCounter}`, 10, 20)
 	}
-	initLearning(ctx, generations)
+	if (ctx !== null) initLearning(ctx, generations)
 }
 
-main(1000)
-
-
-
+main(Infinity, 2000, 20)
