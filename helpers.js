6e1161e4a621c258431421da65018a8adfa9324d
Zam <samuel.kodehode@gmail.com>
2024-03-02 13:17:35 +0100

    2.3.2

---
+++ helpers.js
@@ -0,0 +1,8 @@
+export const getRandomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomColor = () => {
+    const red = getRandomNumberInRange(0, 255);
+    const green = getRandomNumberInRange(0, 255);
+    const blue = getRandomNumberInRange(0, 255);
+    return `rgb(${red},${green},${blue})`;
+};
+//# sourceMappingURL=helpers.js.map
